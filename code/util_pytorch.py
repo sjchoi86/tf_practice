0632907ca9b6ddabd37ac6ba4c1fc93c02803842
jjerry-k <jaeyeol2931@gmail.com>
2020-10-02 01:42:12 +0900

    Edit MLP, CNN Pytorch

--- code/util_pytorch.py
+++ code/util_pytorch.py
@@ -1,37 +1,49 @@
 import torch
 from torch import nn, optim
 from torch.nn import functional as F
+from scipy.stats import truncnorm
 
-def mlp(in_features=3, h_dims=[256,256], actv=nn.ReLU, out_actv=nn.ReLU,
-        USE_DROPOUT=False, device=None):
-    """
-    Multi-layer perceptron 
-    """
-    layers = []
-    in_features = in_features
-    for h_dim in h_dims[:-1]:
-        
-        ki = torch.randn(h_dim, in_features, dtype=torch.float, requires_grad=True)
-        
-        linear = nn.Linear(in_features, h_dim)
-        linear.weight = nn.Parameter(ki)
+
+def truncated_normal(size, threshold=1, dtype=torch.float, requires_grad=True):
+    values = truncnorm.rvs(-threshold, threshold, size=size)
+    values = torch.from_numpy(values).type(dtype)
+    values.requires_grad = requires_grad
+    return values
+
+class mlp(nn.Module):
+    def __init__(self, in_features=784, h_dims=[256, 256], 
+                    actv=nn.ReLU, out_actv=nn.ReLU, USE_DROPOUT=False):
+        """
+        Multi-layer perceptron 
+        """
+        super(mlp, self).__init__()
+
+        layers = []
+        in_features = in_features
+        for h_dim in h_dims[:-1]:
+
+            linear = nn.Linear(in_features, h_dim)
+            # ki = truncated_normal(size=(h_dim, in_features), dtype=torch.float, requires_grad=True)
+            # linear.weight = nn.Parameter(ki)
+            layers.append(linear)
+            
+            act = actv(inplace=True)
+            layers.append(act)
+
+            in_features = h_dim
+            
+            if USE_DROPOUT:
+                layers.append(nn.Dropout())
+        linear = nn.Linear(in_features, h_dims[-1])     
+        # ki = truncated_normal(size=(h_dims[-1], in_features), dtype=torch.float, requires_grad=True)
+        # linear.weight = nn.Parameter(ki)
         layers.append(linear)
         
-        act = actv(inplace=True).to(device)
-        layers.append(act)
-
-        in_features = h_dim
+        if out_actv:
+            act = out_actv(inplace=True)
+            layers.append(act)
         
-        if USE_DROPOUT:
-            layers.append(nn.Dropout())
-                
-    ki = torch.randn(h_dims[-1], in_features, dtype=torch.float, requires_grad=True)
-    linear = nn.Linear(in_features, h_dims[-1])
-    linear.weight = nn.Parameter(ki)
-    layers.append(linear)
+        self.moedl = nn.Sequential(*layers)
     
-    if out_actv:
-        act = out_actv(inplace=True)
-        layers.append(act)
-    
-    return nn.Sequential(*layers).to(device)
+    def forward(self, X):
+        return self.moedl(X)
